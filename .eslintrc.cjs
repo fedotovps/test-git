574bbd361a80f93824bb3e4bf996532ba050db89
fedotovps <fedotovps@bk.ru>
2024-07-26 15:28:29 +0300

    Конфигурация ESlint и Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier", // Отключает правила, конфликтующие с Prettier
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+  rules: {
+    indent: ["error", 2], // Используем отступы в 2 пробела
+    quotes: ["error", "double"], // Используем двойные кавычки
+    semi: ["error", "always"], // Всегда требует точку с запятой в конце строки
+    "prettier/prettier": ["error", { semi: true, singleQuote: false }], // Устанавливает правило для Prettier в ESLint
+  },
+};
